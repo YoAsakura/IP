2cf49cc96fe1f99cdd6744ae622d5ccb440ece0f
Roman Murunov <romamurunov@mail.ru>
2022-03-08 08:25:14 +0300

    123

--- mail.php
+++ mail.php
@@ -1,12 +1,16 @@
 <?php
 $fio = $_POST['fio'];
+$email = $_POST['email'];
 $fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
 $fio = urldecode($fio);
+$email = urldecode($email);
 $fio = trim($fio);
+$email = trim($email);
 //echo $fio;
 //echo "<br>";
 //echo $email;
-if (mail("Данные с сайта", "Заявка с сайта", "Информация:".$fio,"From: romamurunov@mail.ru \r\n"))
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: romamurunov@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
